9432604753b47a5fc6e8d9282203b8b8a79b60b8
the-victim33 <jendmyer@gmail.com>
2025-12-11 14:27:55 -0800

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
 
 dotenv.config();
 
